426b61d3d4aab7638486756c75f9792729012ad6
miyabe <yutaro@dhcp154.sa.cs-private>
2014-07-21 19:03:00 +0900

    finished start page

--- src/com/example/planetarium/MainActivity.java
+++ src/com/example/planetarium/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.planetarium;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.os.Build;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
+	public static String TAG = "MainActivity";
+	
+	public void star(View view){
+		Log.d(TAG, "star");
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
